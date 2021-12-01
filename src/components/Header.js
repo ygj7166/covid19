d2397e2e4baf49a73c9f070b770fe4bd3e5a314a
ygj7166 <ygj7166@naver.com>
2021-12-01 12:27:05 +0900

    covid19

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div className="header-header">
+    <header className="header">
+			<h1>코로나19(COVID-19)</h1>
+    </header>
+    </div>
+  )
+}
+
+export default Header;
